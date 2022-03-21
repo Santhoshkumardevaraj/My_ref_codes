5a0ce5ce9a8bbd304774a757cdf1cc56d927d4e9
Santhosh Kumar Devaraj <39290866+Santhoshkumardevaraj@users.noreply.github.com>
2022-03-21 19:12:18 +0530

    My Jquery scripts

---
+++ Jquery_scripts.js
@@ -0,0 +1,16 @@
+// Textbox validation
+if ($('#<%=TxtbxLanguageskills.ClientID %>').val() == "") {
+                $('#<%=TxtbxLanguageskills.ClientID %>').addClass('ipalert');
+                $('#<%=TxtbxLanguageskills.ClientID %>').focus();
+                showErrormsg('Comments not filled', 'bg-red', 0)
+                return false;
+            }
+
+// Dropdown Validation
+
+ if ($("#<%=DdlLanguage.ClientID%> option:selected").text() == "--Select--") {
+                $('#<%=DdlLanguage.ClientID %>').addClass('ipalert');
+                $('#<%=DdlLanguage.ClientID %>').focus();
+                showErrormsg('Choose Language', 'bg-red', 0)
+                return false;
+            }
